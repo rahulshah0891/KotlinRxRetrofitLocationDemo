dbdcd6068aa33989dd7a941afbdbe366d5e7882e
rahulshah0891 <rahulshah0891@gmail.com>
2018-03-04 21:24:26 +0530

    Working demo of locations

--- app/src/main/java/com/kotlinrxdemo/activity/MainActivity.kt
+++ app/src/main/java/com/kotlinrxdemo/activity/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.kotlinrxdemo
+package com.kotlinrxdemo.activity
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import com.kotlinrxdemo.R
 import com.kotlinrxdemo.retrofit.RetrofitSingleton
 import com.kotlinrxdemo.retrofitbeans.LocationResponseBean
 import io.reactivex.Observable
@@ -14,14 +16,15 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getLocationObs()?.subscribeOn(Schedulers.io())?.observeOn(AndroidSchedulers.mainThread())
+        getLocationObs()?.subscribeOn(Schedulers.io())?.observeOn(AndroidSchedulers.mainThread())?.
+                subscribe({ t: LocationResponseBean -> Log.e("Response", t.status) }, { t: Throwable -> Log.e("Error", t.message) })
 
 
     }
 
     fun getLocationObs(): Observable<LocationResponseBean>? {
         val locationResponseObs = RetrofitSingleton.instance.configRetrofit()?.
-                googlePlaceLocation(getString(R.string.api_key),"23.586761,72.369949", "2000", "restaurant");
+                googlePlaceLocation(getString(R.string.api_key), "23.586761,72.369949", "2000", "restaurant");
         return locationResponseObs
     }
 }
--- app/src/main/java/com/kotlinrxdemo/retrofit/ApiInterface.kt
+++ app/src/main/java/com/kotlinrxdemo/retrofit/ApiInterface.kt
@@ -12,7 +12,7 @@ interface ApiInterface {
     //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=23.586761,72.369949
     // &radius=500&type=restaurant&key=AIzaSyBePPBGTexyu_TamKkmqQT0idaeQaGz5uA
 
-    @GET("json") fun googlePlaceLocation(@Header("key") key: String, @Header("location") location: String, @Header("radius") radius: String, @Header("type") type: String)
-            : Observable<LocationResponseBean>
+    @GET("json") fun googlePlaceLocation(@Query("key") key: String, @Query("location") location: String,
+                                         @Query("radius") radius: String, @Query("type") type: String): Observable<LocationResponseBean>
 
 }
